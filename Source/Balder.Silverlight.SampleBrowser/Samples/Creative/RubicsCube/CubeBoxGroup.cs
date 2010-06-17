6aba32529d0d78e92560f9d8e9c3daa342d4974e
Einar Ingebrigtsen <devnull@localhost>
2010-06-17 12:39:19 +0200

    Rotation of colors and counter clockwize rotation done for Rubiks Cube

--- Source/Balder.Silverlight.SampleBrowser/Samples/Creative/RubicsCube/CubeBoxGroup.cs
+++ Source/Balder.Silverlight.SampleBrowser/Samples/Creative/RubicsCube/CubeBoxGroup.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Balder.Core.Math;
@@ -31,16 +32,66 @@ namespace Balder.Silverlight.SampleBrowser.Samples.Creative.RubicsCube
 
 		public void Snap()
 		{
+			Action rotationAction;
+			double positiveAngle;
+			
+			if( _angle < 0 )
+			{
+				rotationAction = RotateColors;
+				positiveAngle = Math.Abs(_angle);
+			} else
+			{
+				rotationAction = RotateColorsCounterClockWize;
+				
+				positiveAngle = _angle;
+			}
+
+			var rotationCount = (int)((positiveAngle / 90d) + 0.5d);
+
 			foreach (var box in Boxes)
 			{
 				box.Reset();
 			}
-			RotateColors();
+
+			for (var rotation = 0; rotation < rotationCount; rotation++ )
+			{
+				rotationAction();
+			}
+			_angle = 0;
 		}
 
 		private void RotateColors()
 		{
-			var query = from b in _sideBoxes select new { X = b.X, Y = b.Y, Z = b.Z };
+			RotateFront();
+			RotateSides();
+		}
+
+		private void RotateColorsCounterClockWize()
+		{
+			RotateFrontCounterClockWize();
+			RotateSidesCounterClockWize();
+		}
+
+		private void RotateFront()
+		{
+			var c1 = _sideBoxes[10].GetColorForSide(_side);
+			var c2 = _sideBoxes[11].GetColorForSide(_side);
+
+			_sideBoxes[10].SetColorForSide(_side, _sideBoxes[7].GetColorForSide(_side));
+			_sideBoxes[11].SetColorForSide(_side, _sideBoxes[8].GetColorForSide(_side));
+
+			_sideBoxes[7].SetColorForSide(_side, _sideBoxes[4].GetColorForSide(_side));
+			_sideBoxes[8].SetColorForSide(_side, _sideBoxes[5].GetColorForSide(_side));
+
+			_sideBoxes[4].SetColorForSide(_side, _sideBoxes[1].GetColorForSide(_side));
+			_sideBoxes[5].SetColorForSide(_side, _sideBoxes[2].GetColorForSide(_side));
+
+			_sideBoxes[1].SetColorForSide(_side, c1);
+			_sideBoxes[2].SetColorForSide(_side, c2);
+		}
+
+		private void RotateSides()
+		{
 			var c1 = _sideBoxes[10].GetColorForSide(_sideDefinitions[10]);
 			var c2 = _sideBoxes[11].GetColorForSide(_sideDefinitions[11]);
 			var c3 = _sideBoxes[0].GetColorForSide(_sideDefinitions[0]);
@@ -60,23 +111,50 @@ namespace Balder.Silverlight.SampleBrowser.Samples.Creative.RubicsCube
 			_sideBoxes[1].SetColorForSide(_sideDefinitions[1], c1);
 			_sideBoxes[2].SetColorForSide(_sideDefinitions[2], c2);
 			_sideBoxes[3].SetColorForSide(_sideDefinitions[3], c3);
-
-			
-			var fc1 = _sideBoxes[10].GetColorForSide(_side);
-			var fc2 = _sideBoxes[11].GetColorForSide(_side);
-
-			_sideBoxes[10].SetColorForSide(_side, _sideBoxes[7].GetColorForSide(_side));
-			_sideBoxes[11].SetColorForSide(_side, _sideBoxes[8].GetColorForSide(_side));
-
-			_sideBoxes[7].SetColorForSide(_side, _sideBoxes[4].GetColorForSide(_side));
-			_sideBoxes[8].SetColorForSide(_side, _sideBoxes[5].GetColorForSide(_side));
-
-			_sideBoxes[4].SetColorForSide(_side, _sideBoxes[1].GetColorForSide(_side));
-			_sideBoxes[5].SetColorForSide(_side, _sideBoxes[2].GetColorForSide(_side));
-
-			_sideBoxes[1].SetColorForSide(_side, fc1);
-			_sideBoxes[2].SetColorForSide(_side, fc2);
 		}
+
+		private void RotateFrontCounterClockWize()
+		{
+			var c1 = _sideBoxes[1].GetColorForSide(_side);
+			var c2 = _sideBoxes[2].GetColorForSide(_side);
+
+			_sideBoxes[1].SetColorForSide(_side, _sideBoxes[4].GetColorForSide(_side));
+			_sideBoxes[2].SetColorForSide(_side, _sideBoxes[5].GetColorForSide(_side));
+
+			_sideBoxes[4].SetColorForSide(_side, _sideBoxes[7].GetColorForSide(_side));
+			_sideBoxes[5].SetColorForSide(_side, _sideBoxes[8].GetColorForSide(_side));
+
+			_sideBoxes[7].SetColorForSide(_side, _sideBoxes[10].GetColorForSide(_side));
+			_sideBoxes[8].SetColorForSide(_side, _sideBoxes[11].GetColorForSide(_side));
+
+			_sideBoxes[10].SetColorForSide(_side, c1);
+			_sideBoxes[11].SetColorForSide(_side, c2);
+		}
+
+
+		private void RotateSidesCounterClockWize()
+		{
+			var c1 = _sideBoxes[1].GetColorForSide(_sideDefinitions[1]);
+			var c2 = _sideBoxes[2].GetColorForSide(_sideDefinitions[2]);
+			var c3 = _sideBoxes[3].GetColorForSide(_sideDefinitions[3]);
+
+			_sideBoxes[1].SetColorForSide(_sideDefinitions[1], _sideBoxes[4].GetColorForSide(_sideDefinitions[4]));
+			_sideBoxes[2].SetColorForSide(_sideDefinitions[2], _sideBoxes[5].GetColorForSide(_sideDefinitions[5]));
+			_sideBoxes[3].SetColorForSide(_sideDefinitions[3], _sideBoxes[6].GetColorForSide(_sideDefinitions[6]));
+
+			_sideBoxes[4].SetColorForSide(_sideDefinitions[4], _sideBoxes[7].GetColorForSide(_sideDefinitions[7]));
+			_sideBoxes[5].SetColorForSide(_sideDefinitions[5], _sideBoxes[8].GetColorForSide(_sideDefinitions[8]));
+			_sideBoxes[6].SetColorForSide(_sideDefinitions[6], _sideBoxes[9].GetColorForSide(_sideDefinitions[9]));
+
+			_sideBoxes[7].SetColorForSide(_sideDefinitions[7], _sideBoxes[10].GetColorForSide(_sideDefinitions[10]));
+			_sideBoxes[8].SetColorForSide(_sideDefinitions[8], _sideBoxes[11].GetColorForSide(_sideDefinitions[11]));
+			_sideBoxes[9].SetColorForSide(_sideDefinitions[9], _sideBoxes[0].GetColorForSide(_sideDefinitions[0]));
+
+			_sideBoxes[10].SetColorForSide(_sideDefinitions[10], c1);
+			_sideBoxes[11].SetColorForSide(_sideDefinitions[11], c2);
+			_sideBoxes[0].SetColorForSide(_sideDefinitions[0], c3);
+		}
+
 
 		public void Add(CubeBox box, params BoxSide[] sides)
 		{
