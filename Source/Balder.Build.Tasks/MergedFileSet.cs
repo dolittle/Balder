564ab1121527c085db32081716c76b4460c58c08
Einar Ingebrigtsen <devnull@localhost>
2010-07-05 14:04:55 +0200

    Build up and running with unit tests as well - including TypeMock

---
+++ Source/Balder.Build.Tasks/MergedFileSet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NAnt.Core.Attributes;
+using NAnt.Core.Types;
+
+namespace Balder.Build.Tasks
+{
+	[Serializable, ElementName("MergedFileSet")]
+	public class MergedFileSet : FileSet
+	{
+		[BuildElement("fileset")]
+		public void AddFileSetRef(FileSet fileSetRef)
+		{
+			foreach( var include in fileSetRef.Includes )
+			{
+				Includes.Add(include);
+			}
+		}
+	}
+}
