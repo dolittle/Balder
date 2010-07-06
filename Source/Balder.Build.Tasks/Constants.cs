c4bd3959af613ad945df508360b29b4b14470545
Einar Ingebrigtsen <devnull@localhost>
2010-07-06 08:11:59 +0200

    Added LastBuild information for outputing changes.txt document alongside the binaries. The report is basically just a Mercurial dump of changesets since last build. If there are no changes, it won't create the file.

--- Source/Balder.Build.Tasks/Constants.cs
+++ Source/Balder.Build.Tasks/Constants.cs
@@ -5,6 +5,10 @@
 		public const string VersionFile = "version.txt";
 		public const string VersionVariable = "Build.Version";
 		public const string ShouldDeployVariable = "Build.ShouldDeploy";
+		public const string LastBuildFile = "lastbuild.txt";
+		public const string LastBuildVariable = "Build.LastBuild";
+		public const string CurrentDate = "Build.CurrentDate";
 
+		public const string DateFormat = "yyyy-MM-dd HH:mm:ss";
 	}
 }
