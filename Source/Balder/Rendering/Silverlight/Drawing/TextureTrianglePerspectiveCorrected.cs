eda39708af865a43cdf7d6c2590554e9653c8bae
Einar Ingebrigtsen <devnull@localhost>
2010-08-06 12:16:21 +0200

    Triangle now has all that every polygon needs to get drawn - only thing every triangle rendering type need to do is to override the DrawSpan() method.

--- Source/Balder/Rendering/Silverlight/Drawing/TextureTrianglePerspectiveCorrected.cs
+++ Source/Balder/Rendering/Silverlight/Drawing/TextureTrianglePerspectiveCorrected.cs
@@ -20,322 +20,30 @@
 #endregion
 #if(SILVERLIGHT)
 using System;
-using Balder.Math;
 
 namespace Balder.Rendering.Silverlight.Drawing
 {
-	// Based upon : http://www.lysator.liu.se/~mikaelk/doc/perspectivetexture/
 	public class TextureTrianglePerspectiveCorrected : Triangle
 	{
-		private int[] _framebuffer;
-		private uint[] _depthBuffer;
-
-		private int _pixelOffset;
-
-		private int _y1Int;
-		private int _y2Int;
-
-		private float _xInterpolate1;
-		private float _xInterpolate2;
-
-		private TextureMipMapLevel _texture;
-
-		private float _x1;
-		private float _x2;
-		private float _z1;
-		private float _u1;
-		private float _v1;
-
-		private float _zInterpolationX;
-		private float _uzInerpolationX;
-		private float _vzInterpolationX;
-		private float _zInterpolationY;
-		private float _uzInterpolationY;
-		private float _vInterpolationY;
-		private float _zInterpolateY1;
-		private float _uInterpolateY1;
-		private float _vInterpolateY1;
-
-	
-		private static void SetSphericalEnvironmentMapTextureCoordinate(RenderVertex vertex)
+		protected override void DrawSpan(int offset)
 		{
-			var u = vertex.TransformedVectorNormalized;
-			var n = vertex.TransformedNormal;
-			var r = Vector.Reflect(n, u);
-			var m = MathHelper.Sqrt((r.X * r.X) + (r.Y * r.Y) +
-									((r.Z + 0f) * (r.Z + 0f)));
-			var s = (r.X / m);
-			var t = (r.Y / m);
-			vertex.U = (s * 0.5f) + 0.5f;
-			vertex.V = -(t * 0.5f) + 0.5f;
-		}
-
-		public override void Draw(RenderFace face, RenderVertex[] vertices)
-		{
-			var vertexA = vertices[face.A];
-			var vertexB = vertices[face.B];
-			var vertexC = vertices[face.C];
-
-			if (null != face.DiffuseTextureCoordinateA)
-			{
-				vertexA.U = face.DiffuseTextureCoordinateA.U;
-				vertexA.V = face.DiffuseTextureCoordinateA.V;
-			}
-			if (null != face.DiffuseTextureCoordinateB)
-			{
-				vertexB.U = face.DiffuseTextureCoordinateB.U;
-				vertexB.V = face.DiffuseTextureCoordinateB.V;
-			}
-			if (null != face.DiffuseTextureCoordinateC)
-			{
-				vertexC.U = face.DiffuseTextureCoordinateC.U;
-				vertexC.V = face.DiffuseTextureCoordinateC.V;
-			}
-
-
-			_texture = null;
-			if (null != face.DiffuseTexture)
-			{
-				_texture = face.DiffuseTexture.FullDetailLevel;
-			}
-			else if (null != face.ReflectionTexture)
-			{
-				_texture = face.ReflectionTexture.FullDetailLevel;
-				SetSphericalEnvironmentMapTextureCoordinate(vertexA);
-				SetSphericalEnvironmentMapTextureCoordinate(vertexB);
-				SetSphericalEnvironmentMapTextureCoordinate(vertexC);
-			}
-
-			GetSortedPoints(ref vertexA, ref vertexB, ref vertexC);
-
-			var xa = vertexA.TranslatedScreenCoordinates.X;
-			var ya = vertexA.TranslatedScreenCoordinates.Y;
-			var za = vertexA.DepthBufferAdjustedZ;
-			var ua = vertexA.U * _texture.Width;
-			var va = vertexA.V * _texture.Height;
-
-			var xb = vertexB.TranslatedScreenCoordinates.X;
-			var yb = vertexB.TranslatedScreenCoordinates.Y;
-			var zb = vertexB.DepthBufferAdjustedZ;
-			var ub = vertexB.U * _texture.Width;
-			var vb = vertexB.V * _texture.Height;
-
-			var xc = vertexC.TranslatedScreenCoordinates.X;
-			var yc = vertexC.TranslatedScreenCoordinates.Y;
-			var zc = vertexC.DepthBufferAdjustedZ;
-			var uc = vertexC.U * _texture.Width;
-			var vc = vertexC.V * _texture.Height;
-
-			var yaInt = (int)ya;
-			var ybInt = (int)yb;
-			var ycInt = (int)yc;
-
-			if ((yaInt == ybInt && yaInt == ycInt)
-				|| ((int)xa == (int)xb && (int)xa == (int)xc))
-			{
-				return;
-			}
-
-			var deltaXA = xb - xa;
-			var deltaXB = xc - xa;
-			var deltaXC = xc - xb;
-
-			var deltaYA = yb - ya;
-			var deltaYB = yc - ya;
-			var deltaYC = yc - yb;
-
-			var deltaZA = zb - za;
-			var deltaZB = zc - za;
-			var deltaZC = zc - zb;
-
-			var xInterpolateA = deltaXA / deltaYA;
-			var xInterpolateB = deltaXB / deltaYB;
-			var xInterpolateC = deltaXC / deltaYC;
-
-			var oneOverZA = 1f / za;
-			var oneOverZB = 1f / zb;
-			var oneOverZC = 1f / zc;
-
-			var uOneOverZA = ua * oneOverZA;
-			var vOneOverZA = va * oneOverZA;
-
-			var uOneOverZB = ub * oneOverZB;
-			var vOneOverZB = vb * oneOverZB;
-
-			var uOneOverZC = uc * oneOverZC;
-			var vOneOverZC = vc * oneOverZC;
-
-			var denominator = ((xc - xa) * (yb - ya) - (xb - xa) * (yc - ya));
-			if (float.IsInfinity(denominator) || denominator == 0)
-			{
-				return;
-			}
-
-			denominator = 1f / denominator;	
-			_zInterpolationX = ((oneOverZC - oneOverZA) * deltaYA - (oneOverZB - oneOverZA) * deltaYB) * denominator;
-			_uzInerpolationX = ((uOneOverZC - uOneOverZA) * deltaYA - (uOneOverZB - uOneOverZA) * deltaYB) * denominator;
-			_vzInterpolationX = ((vOneOverZC - vOneOverZA) * deltaYA - (vOneOverZB - vOneOverZA) * deltaYB) * denominator;
-			_zInterpolationY = ((oneOverZB - oneOverZA) * deltaXB - (oneOverZC - oneOverZA) * deltaXA) * denominator;
-			_uzInterpolationY = ((uOneOverZB - uOneOverZA) * deltaXB - (uOneOverZC - uOneOverZA) * deltaXA) * denominator;
-			_vInterpolationY = ((vOneOverZB - vOneOverZA) * deltaXB - (vOneOverZC - vOneOverZA) * deltaXA) * denominator;
-
-			_framebuffer = BufferContainer.Framebuffer;
-			_depthBuffer = BufferContainer.DepthBuffer;
-
-			var hypotenuseRight = xInterpolateB > xInterpolateA;
-			if (ya == yb)
-			{
-				hypotenuseRight = xa > xb;
-			}
-			if (yb == yc)
-			{
-				hypotenuseRight = xc > xb;
-			}
-
-			var dy = 0f;
-			dy = 1f - (ya - yaInt);
-			if (!hypotenuseRight)
-			{
-				_xInterpolate1 = xInterpolateB;
-
-				_zInterpolateY1 = xInterpolateB * _zInterpolationX + _zInterpolationY;
-				_uInterpolateY1 = xInterpolateB * _uzInerpolationX + _uzInterpolationY;
-				_vInterpolateY1 = xInterpolateB * _vzInterpolationX + _vInterpolationY;
-
-
-				// Subpixling
-				_x1 = xa + dy * _xInterpolate1;
-
-				_z1 = oneOverZA + dy * _zInterpolateY1;
-				_u1 = uOneOverZA + dy * _uInterpolateY1;
-				_v1 = vOneOverZA + dy * _vInterpolateY1;
-
-
-				if (yaInt < ybInt)
-				{
-					_x2 = xa + dy * xInterpolateA;
-					_xInterpolate2 = xInterpolateA;
-
-					_y1Int = yaInt;
-					_y2Int = ybInt;
-
-					DrawSubTriangleSegment();
-				}
-
-				if (ybInt < ycInt)
-				{
-					_x2 = xb + (1f - (yb - ybInt)) * xInterpolateC;
-					_xInterpolate2 = xInterpolateC;
-
-					_y1Int = ybInt;
-					_y2Int = ycInt;
-
-					DrawSubTriangleSegment();
-				}
-			}
-			else // Hypotenuse is to the right
-			{
-				_xInterpolate2 = xInterpolateB;
-
-				_x2 = xa + dy * xInterpolateB;
-
-				if (yaInt < ybInt)
-				{
-					_xInterpolate1 = xInterpolateA;
-
-					_zInterpolateY1 = _xInterpolate1 * _zInterpolationX + _zInterpolationY;
-					_uInterpolateY1 = _xInterpolate1 * _uzInerpolationX + _uzInterpolationY;
-					_vInterpolateY1 = _xInterpolate1 * _vzInterpolationX + _vInterpolationY;
-
-					// Subpixling
-					_x1 = xa + dy * _xInterpolate1;
-
-					_z1 = oneOverZA + dy * _zInterpolateY1;
-					_u1 = uOneOverZA + dy * _uInterpolateY1;
-					_v1 = vOneOverZA + dy * _vInterpolateY1;
-
-					_y1Int = yaInt;
-					_y2Int = ybInt;
-
-					DrawSubTriangleSegment();
-				}
-				if (ybInt < ycInt)
-				{
-					_xInterpolate1 = xInterpolateC;
-
-					_zInterpolateY1 = xInterpolateC * _zInterpolationX + _zInterpolationY;
-					_uInterpolateY1 = xInterpolateC * _uzInerpolationX + _uzInterpolationY;
-					_vInterpolateY1 = xInterpolateC * _vzInterpolationX + _vInterpolationY;
-
-					dy = 1 - (yb - ybInt);
-
-					// Subpixling
-					_x1 = xb + dy * _xInterpolate1;
-
-					_z1 = oneOverZB + dy * _zInterpolateY1;
-					_u1 = uOneOverZB + dy * _uInterpolateY1;
-					_v1 = vOneOverZB + dy * _vInterpolateY1;
-
-					_y1Int = ybInt;
-					_y2Int = ycInt;
-
-					DrawSubTriangleSegment();
-				}
-			}
-		}
-
-
-
-		private void DrawSubTriangleSegment()
-		{
-			var yoffset = BufferContainer.Width * _y1Int;
-
-			for (var y = _y1Int; y < _y2Int; y++)
-			{
-				if (y >= 0 && y < BufferContainer.Height)
-				{
-					if ((int)_x1 < (int)_x2)
-					{
-						_pixelOffset = yoffset + (int)_x1;
-						DrawSpan(_depthBuffer,
-								 _pixelOffset,
-								 _framebuffer);
-					}
-				}
-				_x1 += _xInterpolate1;
-				_x2 += _xInterpolate2;
-
-				_z1 += _zInterpolateY1;
-				_u1 += _uInterpolateY1;
-				_v1 += _vInterpolateY1;
-
-				yoffset += BufferContainer.Width;
-			}
-		}
-
-
-		protected virtual void DrawSpan(
-			uint[] depthBuffer,
-			int offset,
-			int[] framebuffer)
-		{
-			var textureWidth = _texture.Width;
-			var textureHeight = _texture.Height;
+			var textureWidth = Texture.Width;
+			var textureHeight = Texture.Height;
 
 			float u;
 			float v;
 			float z;
 
-			var dx = 1f - (_x1 - (int)_x1);
-			var zz = _z1 + dx * _zInterpolationX;
-			var uu = _u1 + dx * _uzInerpolationX;
-			var vv = _v1 + dx * _vzInterpolationX;
+			var dx = 1f - (X1 - (int)X1);
+			var zz = Z1 + dx * ZInterpolationX;
+			var uu = U1 + dx * UzInerpolationX;
+			var vv = V1 + dx * VzInterpolationX;
 
 			var color = (uint)0xff000000;
 			var colorAsInt = (int)color;
 
-			var x1Int = (int)_x1;
-			var x2Int = (int)_x2;
+			var x1Int = (int)X1;
+			var x2Int = (int)X2;
 
 			for (var x = x1Int; x < x2Int; x++)
 			{
@@ -343,7 +51,7 @@ namespace Balder.Rendering.Silverlight.Drawing
 				{
 					z = 1f / zz;
 					var bufferZ = (UInt32)((1.0f - z) * (float)UInt32.MaxValue);
-					if (bufferZ > depthBuffer[offset] &&
+					if (bufferZ > DepthBuffer[offset] &&
 						z >= 0f &&
 						z < 1f
 						)
@@ -353,70 +61,18 @@ namespace Balder.Rendering.Silverlight.Drawing
 
 						var intu = (int)(u) & (textureWidth - 1);
 						var intv = (int)(v) & (textureHeight - 1);
-						framebuffer[offset] = _texture.Pixels[intu, intv] | colorAsInt;
+						Framebuffer[offset] = Texture.Pixels[intu, intv] | colorAsInt;
 						//framebuffer[offset] = Bilerp(texture, intu, intv, u, v);
-						depthBuffer[offset] = bufferZ;
+						DepthBuffer[offset] = bufferZ;
 					}
 				}
 
 				offset++;
 
-				zz += _zInterpolationX;
-				uu += _uzInerpolationX;
-				vv += _vzInterpolationX;
+				zz += ZInterpolationX;
+				uu += UzInerpolationX;
+				vv += VzInterpolationX;
 			}
-		}
-
-		private static int redMask;
-		private static int greenMask;
-		private static int blueMask;
-		private static int alphaFull;
-
-		static TextureTrianglePerspectiveCorrected()
-		{
-			uint g = 0xff000000;
-			greenMask = (int)g;
-			redMask = 0x00ff0000;
-			blueMask = 0x00ff0000;
-
-			uint a = 0xff000000;
-			alphaFull = (int)a;
-		}
-
-
-		private int Bilerp(TextureMipMapLevel map, int x, int y, float u, float v)
-		{
-			var h = ((int)(u * 256f)) & 0xff;
-			var i = ((int)(v * 256f)) & 0xff;
-
-			var rightOffset = x + 1;
-			var belowOffset = y + 1;
-
-			if (rightOffset >= map.Width)
-			{
-				rightOffset = map.Width - 1;
-			}
-			if (belowOffset >= map.Height)
-			{
-				belowOffset = map.Height - 1;
-			}
-
-			var cr1 = map.Pixels[x, y] | alphaFull;
-			var cr2 = map.Pixels[rightOffset, y] | alphaFull;
-			var cr3 = map.Pixels[rightOffset, belowOffset] | alphaFull;
-			var cr4 = map.Pixels[x, belowOffset] | alphaFull;
-
-			var a = (0x100 - h) * (0x100 - i);
-			var b = (0x000 + h) * (0x100 - i);
-			var c = (0x000 + h) * (0x000 + i);
-			var d = 65536 - a - b - c;
-
-			int red = redMask & (((cr1 >> 16) * a) + ((cr2 >> 16) * b) + ((cr3 >> 16) * c) + ((cr4 >> 16) * d));
-			int green = greenMask & (((cr1 & 0x0000ff00) * a) + ((cr2 & 0x000000ff00) * b) + ((cr3 & 0x0000ff00) * c) + ((cr4 & 0x0000ff00) * d));
-			int blue = blueMask & (((cr1 & 0x000000ff) * a) + ((cr2 & 0x000000ff) * b) + ((cr3 & 0x000000ff) * c) + ((cr4 & 0x000000ff) * d));
-
-			var pixel = red | (((green | blue) >> 16) & 0xffff) | alphaFull;
-			return pixel;
 		}
 	}
 }
--- Source/Balder/Rendering/Silverlight/Drawing/Triangle.cs
+++ Source/Balder/Rendering/Silverlight/Drawing/Triangle.cs
@@ -19,10 +19,73 @@
 
 #endregion
 #if(SILVERLIGHT)
+using Balder.Math;
+
+
 namespace Balder.Rendering.Silverlight.Drawing
 {
 	public abstract class Triangle
 	{
+		protected int[] Framebuffer;
+		protected uint[] DepthBuffer;
+
+		protected int PixelOffset;
+
+		protected int Y1Int;
+		protected int Y2Int;
+
+		protected float XInterpolate1;
+		protected float XInterpolate2;
+
+		protected TextureMipMapLevel Texture;
+
+		protected float X1;
+		protected float X2;
+		protected float Z1;
+		protected float U1;
+		protected float V1;
+
+		protected float ZInterpolationX;
+		protected float UzInerpolationX;
+		protected float VzInterpolationX;
+		protected float ZInterpolationY;
+		protected float UzInterpolationY;
+		protected float VInterpolationY;
+		protected float ZInterpolateY1;
+		protected float UInterpolateY1;
+		protected float VInterpolateY1;
+
+
+		protected static int RedMask;
+		protected static int GreenMask;
+		protected static int BlueMask;
+		protected static int AlphaFull;
+
+		static Triangle()
+		{
+			uint g = 0xff000000;
+			GreenMask = (int)g;
+			RedMask = 0x00ff0000;
+			BlueMask = 0x00ff0000;
+
+			uint a = 0xff000000;
+			AlphaFull = (int)a;
+		}
+
+		protected static void SetSphericalEnvironmentMapTextureCoordinate(RenderVertex vertex)
+		{
+			var u = vertex.TransformedVectorNormalized;
+			var n = vertex.TransformedNormal;
+			var r = Vector.Reflect(n, u);
+			var m = MathHelper.Sqrt((r.X * r.X) + (r.Y * r.Y) +
+									((r.Z + 0f) * (r.Z + 0f)));
+			var s = (r.X / m);
+			var t = (r.Y / m);
+			vertex.U = (s * 0.5f) + 0.5f;
+			vertex.V = -(t * 0.5f) + 0.5f;
+		}
+
+
 		protected void GetSortedPoints(ref RenderVertex vertexA,
 										ref RenderVertex vertexB,
 										ref RenderVertex vertexC)
@@ -109,7 +172,287 @@ namespace Balder.Rendering.Silverlight.Drawing
 			vertexC = point3;
 		}
 
-		public abstract void Draw(RenderFace face, RenderVertex[] vertices);
+
+		protected int Bilerp(TextureMipMapLevel map, int x, int y, float u, float v)
+		{
+			var h = ((int)(u * 256f)) & 0xff;
+			var i = ((int)(v * 256f)) & 0xff;
+
+			var rightOffset = x + 1;
+			var belowOffset = y + 1;
+
+			if (rightOffset >= map.Width)
+			{
+				rightOffset = map.Width - 1;
+			}
+			if (belowOffset >= map.Height)
+			{
+				belowOffset = map.Height - 1;
+			}
+
+			var cr1 = map.Pixels[x, y] | AlphaFull;
+			var cr2 = map.Pixels[rightOffset, y] | AlphaFull;
+			var cr3 = map.Pixels[rightOffset, belowOffset] | AlphaFull;
+			var cr4 = map.Pixels[x, belowOffset] | AlphaFull;
+
+			var a = (0x100 - h) * (0x100 - i);
+			var b = (0x000 + h) * (0x100 - i);
+			var c = (0x000 + h) * (0x000 + i);
+			var d = 65536 - a - b - c;
+
+			int red = RedMask & (((cr1 >> 16) * a) + ((cr2 >> 16) * b) + ((cr3 >> 16) * c) + ((cr4 >> 16) * d));
+			int green = GreenMask & (((cr1 & 0x0000ff00) * a) + ((cr2 & 0x000000ff00) * b) + ((cr3 & 0x0000ff00) * c) + ((cr4 & 0x0000ff00) * d));
+			int blue = BlueMask & (((cr1 & 0x000000ff) * a) + ((cr2 & 0x000000ff) * b) + ((cr3 & 0x000000ff) * c) + ((cr4 & 0x000000ff) * d));
+
+			var pixel = red | (((green | blue) >> 16) & 0xffff) | AlphaFull;
+			return pixel;
+		}
+
+
+		public virtual void Draw(RenderFace face, RenderVertex[] vertices)
+		{
+			var vertexA = vertices[face.A];
+			var vertexB = vertices[face.B];
+			var vertexC = vertices[face.C];
+
+			if (null != face.DiffuseTextureCoordinateA)
+			{
+				vertexA.U = face.DiffuseTextureCoordinateA.U;
+				vertexA.V = face.DiffuseTextureCoordinateA.V;
+			}
+			if (null != face.DiffuseTextureCoordinateB)
+			{
+				vertexB.U = face.DiffuseTextureCoordinateB.U;
+				vertexB.V = face.DiffuseTextureCoordinateB.V;
+			}
+			if (null != face.DiffuseTextureCoordinateC)
+			{
+				vertexC.U = face.DiffuseTextureCoordinateC.U;
+				vertexC.V = face.DiffuseTextureCoordinateC.V;
+			}
+
+
+			Texture = null;
+			if (null != face.DiffuseTexture)
+			{
+				Texture = face.DiffuseTexture.FullDetailLevel;
+			}
+			else if (null != face.ReflectionTexture)
+			{
+				Texture = face.ReflectionTexture.FullDetailLevel;
+				SetSphericalEnvironmentMapTextureCoordinate(vertexA);
+				SetSphericalEnvironmentMapTextureCoordinate(vertexB);
+				SetSphericalEnvironmentMapTextureCoordinate(vertexC);
+			}
+
+			GetSortedPoints(ref vertexA, ref vertexB, ref vertexC);
+
+			var xa = vertexA.TranslatedScreenCoordinates.X;
+			var ya = vertexA.TranslatedScreenCoordinates.Y;
+			var za = vertexA.DepthBufferAdjustedZ;
+			var ua = vertexA.U * Texture.Width;
+			var va = vertexA.V * Texture.Height;
+
+			var xb = vertexB.TranslatedScreenCoordinates.X;
+			var yb = vertexB.TranslatedScreenCoordinates.Y;
+			var zb = vertexB.DepthBufferAdjustedZ;
+			var ub = vertexB.U * Texture.Width;
+			var vb = vertexB.V * Texture.Height;
+
+			var xc = vertexC.TranslatedScreenCoordinates.X;
+			var yc = vertexC.TranslatedScreenCoordinates.Y;
+			var zc = vertexC.DepthBufferAdjustedZ;
+			var uc = vertexC.U * Texture.Width;
+			var vc = vertexC.V * Texture.Height;
+
+			var yaInt = (int)ya;
+			var ybInt = (int)yb;
+			var ycInt = (int)yc;
+
+			if ((yaInt == ybInt && yaInt == ycInt)
+				|| ((int)xa == (int)xb && (int)xa == (int)xc))
+			{
+				return;
+			}
+
+			var deltaXA = xb - xa;
+			var deltaXB = xc - xa;
+			var deltaXC = xc - xb;
+
+			var deltaYA = yb - ya;
+			var deltaYB = yc - ya;
+			var deltaYC = yc - yb;
+
+			var deltaZA = zb - za;
+			var deltaZB = zc - za;
+			var deltaZC = zc - zb;
+
+			var xInterpolateA = deltaXA / deltaYA;
+			var xInterpolateB = deltaXB / deltaYB;
+			var xInterpolateC = deltaXC / deltaYC;
+
+			var oneOverZA = 1f / za;
+			var oneOverZB = 1f / zb;
+			var oneOverZC = 1f / zc;
+
+			var uOneOverZA = ua * oneOverZA;
+			var vOneOverZA = va * oneOverZA;
+
+			var uOneOverZB = ub * oneOverZB;
+			var vOneOverZB = vb * oneOverZB;
+
+			var uOneOverZC = uc * oneOverZC;
+			var vOneOverZC = vc * oneOverZC;
+
+			var denominator = ((xc - xa) * (yb - ya) - (xb - xa) * (yc - ya));
+			if (float.IsInfinity(denominator) || denominator == 0)
+			{
+				return;
+			}
+
+			denominator = 1f / denominator;
+			ZInterpolationX = ((oneOverZC - oneOverZA) * deltaYA - (oneOverZB - oneOverZA) * deltaYB) * denominator;
+			UzInerpolationX = ((uOneOverZC - uOneOverZA) * deltaYA - (uOneOverZB - uOneOverZA) * deltaYB) * denominator;
+			VzInterpolationX = ((vOneOverZC - vOneOverZA) * deltaYA - (vOneOverZB - vOneOverZA) * deltaYB) * denominator;
+			ZInterpolationY = ((oneOverZB - oneOverZA) * deltaXB - (oneOverZC - oneOverZA) * deltaXA) * denominator;
+			UzInterpolationY = ((uOneOverZB - uOneOverZA) * deltaXB - (uOneOverZC - uOneOverZA) * deltaXA) * denominator;
+			VInterpolationY = ((vOneOverZB - vOneOverZA) * deltaXB - (vOneOverZC - vOneOverZA) * deltaXA) * denominator;
+
+			Framebuffer = BufferContainer.Framebuffer;
+			DepthBuffer = BufferContainer.DepthBuffer;
+
+			var hypotenuseRight = xInterpolateB > xInterpolateA;
+			if (ya == yb)
+			{
+				hypotenuseRight = xa > xb;
+			}
+			if (yb == yc)
+			{
+				hypotenuseRight = xc > xb;
+			}
+
+			var dy = 0f;
+			dy = 1f - (ya - yaInt);
+			if (!hypotenuseRight)
+			{
+				XInterpolate1 = xInterpolateB;
+
+				ZInterpolateY1 = xInterpolateB * ZInterpolationX + ZInterpolationY;
+				UInterpolateY1 = xInterpolateB * UzInerpolationX + UzInterpolationY;
+				VInterpolateY1 = xInterpolateB * VzInterpolationX + VInterpolationY;
+
+
+				// Subpixling
+				X1 = xa + dy * XInterpolate1;
+
+				Z1 = oneOverZA + dy * ZInterpolateY1;
+				U1 = uOneOverZA + dy * UInterpolateY1;
+				V1 = vOneOverZA + dy * VInterpolateY1;
+
+
+				if (yaInt < ybInt)
+				{
+					X2 = xa + dy * xInterpolateA;
+					XInterpolate2 = xInterpolateA;
+
+					Y1Int = yaInt;
+					Y2Int = ybInt;
+
+					DrawSubTriangleSegment();
+				}
+
+				if (ybInt < ycInt)
+				{
+					X2 = xb + (1f - (yb - ybInt)) * xInterpolateC;
+					XInterpolate2 = xInterpolateC;
+
+					Y1Int = ybInt;
+					Y2Int = ycInt;
+
+					DrawSubTriangleSegment();
+				}
+			}
+			else // Hypotenuse is to the right
+			{
+				XInterpolate2 = xInterpolateB;
+
+				X2 = xa + dy * xInterpolateB;
+
+				if (yaInt < ybInt)
+				{
+					XInterpolate1 = xInterpolateA;
+
+					ZInterpolateY1 = XInterpolate1 * ZInterpolationX + ZInterpolationY;
+					UInterpolateY1 = XInterpolate1 * UzInerpolationX + UzInterpolationY;
+					VInterpolateY1 = XInterpolate1 * VzInterpolationX + VInterpolationY;
+
+					// Subpixling
+					X1 = xa + dy * XInterpolate1;
+
+					Z1 = oneOverZA + dy * ZInterpolateY1;
+					U1 = uOneOverZA + dy * UInterpolateY1;
+					V1 = vOneOverZA + dy * VInterpolateY1;
+
+					Y1Int = yaInt;
+					Y2Int = ybInt;
+
+					DrawSubTriangleSegment();
+				}
+				if (ybInt < ycInt)
+				{
+					XInterpolate1 = xInterpolateC;
+
+					ZInterpolateY1 = xInterpolateC * ZInterpolationX + ZInterpolationY;
+					UInterpolateY1 = xInterpolateC * UzInerpolationX + UzInterpolationY;
+					VInterpolateY1 = xInterpolateC * VzInterpolationX + VInterpolationY;
+
+					dy = 1 - (yb - ybInt);
+
+					// Subpixling
+					X1 = xb + dy * XInterpolate1;
+
+					Z1 = oneOverZB + dy * ZInterpolateY1;
+					U1 = uOneOverZB + dy * UInterpolateY1;
+					V1 = vOneOverZB + dy * VInterpolateY1;
+
+					Y1Int = ybInt;
+					Y2Int = ycInt;
+
+					DrawSubTriangleSegment();
+				}
+			}
+		}
+
+
+
+		private void DrawSubTriangleSegment()
+		{
+			var yoffset = BufferContainer.Width * Y1Int;
+
+			for (var y = Y1Int; y < Y2Int; y++)
+			{
+				if (y >= 0 && y < BufferContainer.Height)
+				{
+					if ((int)X1 < (int)X2)
+					{
+						PixelOffset = yoffset + (int)X1;
+						DrawSpan(PixelOffset);
+					}
+				}
+				X1 += XInterpolate1;
+				X2 += XInterpolate2;
+
+				Z1 += ZInterpolateY1;
+				U1 += UInterpolateY1;
+				V1 += VInterpolateY1;
+
+				yoffset += BufferContainer.Width;
+			}
+		}
+
+		protected virtual void DrawSpan(int offset)
+		{
+		}
 	}
 }
 #endif
