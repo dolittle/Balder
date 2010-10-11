a65412e8de6c6a71c9874521484665da7d31aca5
Einar Ingebrigtsen <devnull@localhost>
2010-10-11 09:07:23 +0200

    Moved MouseInfo class to seperate file

---
+++ Source/Balder/Input/Silverlight/MouseInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using Balder.Extensions.Silverlight;
+
+namespace Balder.Input.Silverlight
+{
+	public class MouseInfo : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged = (s, e) => { };
+
+		private int _xPosition;
+		public int XPosition
+		{
+			get { return _xPosition; }
+			set
+			{
+				_xPosition = value;
+				PropertyChanged.Notify(() => XPosition);
+			}
+		}
+
+		private int _yPosition;
+		public int YPosition
+		{
+			get { return _yPosition; }
+			set
+			{
+				_yPosition = value;
+				PropertyChanged.Notify(() => YPosition);
+			}
+		}
+	}
+}
