7604e2ef3f5ea4b7f6318feb7e4acd6ad490151d
Einar Ingebrigtsen <devnull@localhost>
2010-10-18 14:52:50 +0200

    Added a sample that dynamically adds content from an event on a button

---
+++ Source/Balder.Silverlight.SampleBrowser/Samples/Programatic/Dynamic/Content.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+using Balder.Math;
+using Balder.Objects.Geometries;
+
+namespace Balder.Silverlight.SampleBrowser.Samples.Programatic.Dynamic
+{
+	public partial class Content
+	{
+		public Content()
+		{
+			InitializeComponent();
+		}
+
+		private static readonly Random rnd = new Random();
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			var position = new Coordinate((rnd.NextDouble()*10)-5, (rnd.NextDouble()*10)-5, (rnd.NextDouble()*10)-5);
+			var box = new Box {Position = position, Dimension = new Coordinate(5, 5, 5), InteractionEnabled = true};
+			Game.Scene.AddNode(box);
+		}
+	}
+}
