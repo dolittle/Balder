24925a76a433b61fc9a4201ce1d7acd381605da0
Einar Ingebrigtsen <devnull@localhost>
2011-02-11 13:24:14 +0100

    Testing out different things

--- Source/Balder.WP7.TestApp/MainPage.xaml.cs
+++ Source/Balder.WP7.TestApp/MainPage.xaml.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Windows.Threading;
+using Balder.Math;
 using Microsoft.Phone.Controls;
 
 namespace Balder.WP7.TestApp
 {
+	public class Something
+	{
+		public Coordinate Position { get; set; }
+	}
+
+
     public partial class MainPage
     {
         // Constructor
@@ -15,11 +23,21 @@ namespace Balder.WP7.TestApp
             timer.Interval = TimeSpan.FromMilliseconds(20);
             timer.Tick += new EventHandler(timer_Tick);
             timer.Start();
+
+			Items = new ObservableCollection<Something>();
+        	Nodes.ItemsSource = Items;
         }
 
         void timer_Tick(object sender, EventArgs e)
         {
-            Object.Rotation.Y += 1;
+            //Object.Rotation.Y += 1;
         }
+
+		public ObservableCollection<Something> Items { get; set; }
+
+		private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			Items.Add(new Something());
+		}
     }
 }
