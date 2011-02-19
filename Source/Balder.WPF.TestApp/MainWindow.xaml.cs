e7ce3ecd6fba07449d734ef71abe4ed42b7a4b94
Einar Ingebrigtsen <devnull@localhost>
2011-02-19 07:35:28 +0100

    WPF Support up and running - it will need some adjustments, since its now hard-coded to 800*600 in size.

--- Source/Balder.WPF.TestApp/MainWindow.xaml.cs
+++ Source/Balder.WPF.TestApp/MainWindow.xaml.cs
@@ -1,27 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Media;
 
 namespace Balder.WPF.TestApp
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
+	public partial class MainWindow
 	{
 		public MainWindow()
 		{
 			InitializeComponent();
+
+            CompositionTarget.Rendering += CompositionTarget_Rendering;
 		}
+
+        void CompositionTarget_Rendering(object sender, System.EventArgs e)
+        {
+            _box.Rotation.Y += 0.5;
+        }
 	}
 }
