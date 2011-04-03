912e01e236aeaa8ec2d76f934c3f41525d2cace7
Marius <Marius@MariusPC.informaticha.ro>
2011-04-03 13:56:25 +0300

    Modified

---
+++ Source/Balder.Silverlight.SampleBrowser/Samples/VertexCustom/VertexCustom/Content.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Windows.Threading;
+using Balder.Tools;
+using System.Windows.Controls;
+using System.Windows;
+using Balder.Materials;
+using Balder.Math;
+using Balder.Objects.Geometries;
+using System.Collections.Generic;
+using Balder.View;
+
+namespace Balder.Silverlight.SampleBrowser.Samples.VertexCustom.VertexCustom
+{
+    public partial class Content
+    {
+        VertexCustom vtxCuston;
+        TextBox txtVX1;
+        TextBox txtVY1;
+        TextBox txtVZ1;
+        TextBox txtVX2;
+        TextBox txtVY2;
+        TextBox txtVZ2;
+
+        public Content()
+        {
+            InitializeComponent();
+
+            var timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(20);
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+           
+            vtxCuston = new VertexCustom { Position = new Coordinate(0, 0, 0), Rotation = new Coordinate(-45, -45, 0), InteractionEnabled = true, Color = Colors.Red};
+            Game.Scene.AddNode(vtxCuston);
+            vtxCuston.GenerateVertices(126, 2, new Vertex(-5, 3, -2), new Vertex(2, 5, 4));
+            vtxCuston.GenerateLines(63, 1);
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            vtxCuston.Rotation.Y += 0.5f;
+        }
+
+        private static readonly Random rnd = new Random();
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            lstBox.Items.Clear();
+            lstBox.Items.Add(string.Format("Vertex: {0}", vtxCuston.FullDetailLevel.VertexCount.ToString()));
+            lstBox.Items.Add(string.Format("Lines: {0}", vtxCuston.FullDetailLevel.LineCount.ToString()));
+            lstBox.Items.Add(string.Format("Faces: {0}", vtxCuston.FullDetailLevel.FaceCount.ToString()));
+        }
+
+        private void btnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                vtxCuston.GenerateVertices(126, 2, new Vertex(Convert.ToInt32(txtVX1.Text), Convert.ToInt32(txtVY1.Text), Convert.ToInt32(txtVZ1.Text)), new Vertex(Convert.ToInt32(txtVX2.Text), Convert.ToInt32(txtVY2.Text), Convert.ToInt32(txtVZ2.Text)));
+                vtxCuston.GenerateLines(63, 1);
+            }
+            catch (Exception ex)
+            {
+                txtException.Text = ex.Message;
+            }
+            
+        }
+
+        private void spValueV1_Loaded(object sender, RoutedEventArgs e)
+        {
+            StackPanel stackPanel = (sender as StackPanel);
+
+            TextBlock infoV1X = new TextBlock();
+            infoV1X.Text = "V1-X:";
+            TextBlock infoV1Y = new TextBlock();
+            infoV1Y.Text = "V1-Y:";
+            TextBlock infoV1Z = new TextBlock();
+            infoV1Z.Text = "V1-Z:";
+            txtVX1 = new TextBox();
+            txtVX1.Text = "-5";
+            txtVY1 = new TextBox();
+            txtVY1.Text = "3";
+            txtVZ1 = new TextBox();
+            txtVZ1.Text = "-2";
+
+            stackPanel.Children.Add(infoV1X);
+            stackPanel.Children.Add(txtVX1);
+            stackPanel.Children.Add(infoV1Y);
+            stackPanel.Children.Add(txtVY1);
+            stackPanel.Children.Add(infoV1Z);
+            stackPanel.Children.Add(txtVZ1);
+        }
+
+        private void spValueV2_Loaded(object sender, RoutedEventArgs e)
+        {
+            StackPanel stackPanel = (sender as StackPanel);
+
+            TextBlock infoV2X = new TextBlock();
+            infoV2X.Text = "V2-X:";
+            TextBlock infoV2Y = new TextBlock();
+            infoV2Y.Text = "V2-Y:";
+            TextBlock infoV2Z = new TextBlock();
+            infoV2Z.Text = "V2-Z:";
+            txtVX2 = new TextBox();
+            txtVX2.Text = "2";
+            txtVY2 = new TextBox();
+            txtVY2.Text = "5";
+            txtVZ2 = new TextBox();
+            txtVZ2.Text = "4";
+
+            stackPanel.Children.Add(infoV2X);
+            stackPanel.Children.Add(txtVX2);
+            stackPanel.Children.Add(infoV2Y);
+            stackPanel.Children.Add(txtVY2);
+            stackPanel.Children.Add(infoV2Z);
+            stackPanel.Children.Add(txtVZ2);
+        }
+    }
+}
---
+++ Source/Balder.Silverlight.SampleBrowser/Samples/VertexCustom/VertexCustom/VertexCustom.cs
@@ -0,0 +1,121 @@
+﻿using System.Windows.Threading;
+using System.Windows;
+using Balder.Materials;
+using Balder.Math;
+using Balder.Objects.Geometries;
+using Balder.Tools;
+using System;
+using System.Collections.Generic;
+using Balder.View;
+
+namespace Balder.Silverlight.SampleBrowser.Samples.VertexCustom.VertexCustom
+{
+    public class VertexCustom : Geometry
+    {
+        public VertexCustom()
+        {
+        }
+
+        public override void Prepare(Display.Viewport viewport)
+        {
+            base.Prepare(viewport);
+        }
+
+        public void GenerateVertices(int nrVertex, int vtxCustom, Vertex vtP1, Vertex vtP2)
+        {
+            var xAxisStart = new Vertex(-10, 0, 0);
+            var xAxisEnd = new Vertex(10, 0, 0);
+            var yAxisStart = new Vertex(0, -10, 0);
+            var yAxisEnd = new Vertex(0, 10, 0);
+            var zAxisStart = new Vertex(0, 0, -10);
+            var zAxisEnd = new Vertex(0, 0, 10);
+
+            var vtxP1 = vtP1;
+            var vtxP2 = vtP2;
+
+            Dictionary<int, Vertex> vertexAxisXN = new Dictionary<int, Vertex>();
+            Dictionary<int, Vertex> vertexAxisXP = new Dictionary<int, Vertex>();
+            Dictionary<int, Vertex> vertexAxisYP = new Dictionary<int, Vertex>();
+            Dictionary<int, Vertex> vertexAxisYN = new Dictionary<int, Vertex>();
+            Dictionary<int, Vertex> vertexAxisZP = new Dictionary<int, Vertex>();
+            Dictionary<int, Vertex> vertexAxisZN = new Dictionary<int, Vertex>();
+
+            int m = 0, n = 0;
+
+            for (int i = 1, j = 0; i <= 20; i++, j++)
+            {
+                if (i % 2 == 0)
+                    m++;
+                else
+                    n++;
+
+                vertexAxisXN.Add(j, new Vertex(i % 2 == 0 ? -m : -n, i % 2 == 0 ? 0.2f : -0.2f, 0));
+                vertexAxisXP.Add(j, new Vertex(i % 2 == 0 ? m : n, i % 2 == 0 ? 0.2f : -0.2f, 0));
+
+                vertexAxisYN.Add(j, new Vertex(i % 2 == 0 ? 0.2f : -0.2f, i % 2 == 0 ? -m : -n, 0));
+                vertexAxisYP.Add(j, new Vertex(i % 2 == 0 ? 0.2f : -0.2f, i % 2 == 0 ? m : n, 0));
+
+                vertexAxisZN.Add(j, new Vertex(i % 2 == 0 ? 0.2f : -0.2f, 0, i % 2 == 0 ? -m : -n));
+                vertexAxisZP.Add(j, new Vertex(i % 2 == 0 ? 0.2f : -0.2f, 0, i % 2 == 0 ? m : n));
+            }
+
+            FullDetailLevel.AllocateVertices(nrVertex + vtxCustom);
+            FullDetailLevel.SetVertex(0, xAxisStart);
+            FullDetailLevel.SetVertex(1, xAxisEnd);
+            FullDetailLevel.SetVertex(2, yAxisStart);
+            FullDetailLevel.SetVertex(3, yAxisEnd);
+            FullDetailLevel.SetVertex(4, zAxisStart);
+            FullDetailLevel.SetVertex(5, zAxisEnd);
+
+            Dictionary<int, Vertex>.Enumerator enumDicXN = vertexAxisXN.GetEnumerator();
+            while (enumDicXN.MoveNext())
+            {
+                FullDetailLevel.SetVertex(((KeyValuePair<int, Vertex>)(enumDicXN.Current)).Key + 6, ((KeyValuePair<int, Vertex>)(enumDicXN.Current)).Value);
+            }
+
+            foreach (var pair in vertexAxisXP)
+            {
+                FullDetailLevel.SetVertex((pair.Key) + 26, pair.Value);
+            }
+
+            Dictionary<int, Vertex>.Enumerator enumDicYN = vertexAxisYN.GetEnumerator();
+            while (enumDicYN.MoveNext())
+            {
+                FullDetailLevel.SetVertex(((KeyValuePair<int, Vertex>)(enumDicYN.Current)).Key + 46, ((KeyValuePair<int, Vertex>)(enumDicYN.Current)).Value);
+            }
+
+            foreach (var pair in vertexAxisYP)
+            {
+                FullDetailLevel.SetVertex((pair.Key) + 66, pair.Value);
+            }
+
+            Dictionary<int, Vertex>.Enumerator enumDicZN = vertexAxisZN.GetEnumerator();
+            while (enumDicZN.MoveNext())
+            {
+                FullDetailLevel.SetVertex(((KeyValuePair<int, Vertex>)(enumDicZN.Current)).Key + 86, ((KeyValuePair<int, Vertex>)(enumDicZN.Current)).Value);
+            }
+
+            foreach (var pair in vertexAxisZP)
+            {
+                FullDetailLevel.SetVertex((pair.Key) + 106, pair.Value);
+            }
+
+            FullDetailLevel.SetVertex(126, vtxP1);
+            FullDetailLevel.SetVertex(127, vtxP2);
+        }
+
+        public void GenerateLines(int nrLines, int nrCustom)
+        {
+            FullDetailLevel.AllocateLines(nrLines + nrCustom);
+
+            int k = 0, j = 1;
+
+            for (int i = 0; i < (nrLines + nrCustom); i++)
+            {
+                FullDetailLevel.SetLine(i, new Line(k, j));
+                k += 2;
+                j += 2;
+            }
+        }
+    }
+}
