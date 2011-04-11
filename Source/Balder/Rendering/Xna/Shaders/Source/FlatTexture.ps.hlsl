c82ad862baed2c7ff78064435cfa4429aa769887
Einar Ingebrigtsen <devnull@localhost>
2011-04-11 21:19:27 +0200

    FlatTexture up and running!

--- Source/Balder/Rendering/Xna/Shaders/Source/FlatTexture.ps.hlsl
+++ Source/Balder/Rendering/Xna/Shaders/Source/FlatTexture.ps.hlsl
@@ -1,8 +1,20 @@
 #include "Lighting.ps.hlsl"
-#include "Flat.VertexOutput"
+#include "FlatTexture.VertexOutput"
+
+sampler2D TextureSampler = sampler_state
+{
+    Texture = <Texture>;
+    MipFilter = Linear;
+    MinFilter = Anisotropic;
+    MagFilter = Anisotropic;
+    AddressU = Clamp;
+    AddressV = Clamp;
+};
 
 
 float4 main(VertexShaderOutput vertex) : COLOR
 {
-	return vertex.Color;
+	float4 texel = tex2D(TextureSampler, vertex.UV).rgba;
+
+	return (vertex.Diffuse*texel) + vertex.Specular;
 }
--- Source/Balder/Rendering/Xna/Shaders/Source/FlatTexture.vs.hlsl
+++ Source/Balder/Rendering/Xna/Shaders/Source/FlatTexture.vs.hlsl
@@ -1,20 +1,19 @@
 #include "Defaults.hlsl"
 #include "RenderVertex.hlsl"
 #include "Material.hlsl"
-#include "Flat.VertexOutput"
+#include "FlatTexture.VertexOutput"
 #include "Lighting.vs.hlsl"
-
-
-
 
 VertexShaderOutput main(RenderVertex vertex)
 {
 	VertexShaderOutput output;
 
 	float4 position = mul(float4(vertex.Position,1), WorldViewProj);
-	float4 normal = mul(float4(vertex.Normal,1), WorldView);
+	float4 normal = mul(vertex.FaceNormal, WorldView);
 	output.Position = position;
-	output.Color = CalculateLighting(position, normal);
+	output.Diffuse = CalculateDiffuse(position, normal);
+	output.Specular = CalculateSpecular(position, normal);
+	output.UV = vertex.UV.xy;
 
 	return output;
 }
